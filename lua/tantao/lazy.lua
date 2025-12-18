fcc8cf5be2677579ae7205c169c399f64afc033b
Claudio <claudio@cartorionovalia.com.br>
2025-12-18 17:28:33 -0300

    lsp - mason

--- lua/tantao/lazy.lua
+++ lua/tantao/lazy.lua
@@ -14,7 +14,7 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup(
   {
     spec = {
-      { import = "tantao.plugins" }
+      { import = "tantao.plugins" },
       { import = "tantao.plugins.lsp" }
     },
     checker = {
